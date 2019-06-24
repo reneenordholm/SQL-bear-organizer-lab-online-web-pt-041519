be1050e922ce58d8edc39c079cb42007aff97d7d
Renee Nordholm <reneenordholm@gmail.com>
2019-06-24 01:54:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT gender FROM bears WHERE gender = 'F' ORDER by bears.name, bears.age;"
+  "SELECT bears.name, bears.age FROM bears WHERE gender = 'F' ORDER by bears.name, bears.age;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
